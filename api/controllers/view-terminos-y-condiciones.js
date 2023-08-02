4c2c7e9d1575553c3fce45c7e829fa2bdfc54fca
Ezequielprograma <ezequiel.medina.profesional@gmail.com>
2023-08-01 23:18:46 -0300

    Ejercicio 1

---
+++ api/controllers/view-terminos-y-condiciones.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View terminos y condiciones',
+
+
+  description: 'Display "Terminos y condiciones" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/terminos-y-condiciones'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
